a9f853b9e3922deb91cfb988889d47934c701913
kim-eun-ji <qwert101311@gmail.com>
2021-04-08 15:12:59 +0900

    MOD configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,24 +1,21 @@
 module.exports = {
-   "type": "mysql",
-   "host": process.env.DB_HOST,
-   "port": process.env.DB_PORT,
-   "username": process.env.DB_USER,
-   "password": process.env.DB_PASS,
-   "database": process.env.DB_NAME,
-   "synchronize": true,
-   "logging": false,
-   "entities": [
-      "src/entity/**/*.ts"
-   ],
-   "migrations": [
-      "src/migration/**/*.ts"
-   ],
-   "subscribers": [
-      "src/subscriber/**/*.ts"
-   ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration",
-      "subscribersDir": "src/subscriber"
-   }
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  extra: {
+    charset: 'utf8mb4_unicode_ci',
+  },
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
 };
--- src/routes/todo-route.ts
+++ src/routes/todo-route.ts
@@ -6,10 +6,10 @@ const router: Router = Router();
 const todoController = new TodoController(new TodoService());
 
 router.get('/', (req, res) => {
-  res.send('test입니다~');
+  res.send('mashup-todolist2');
 });
 router.get('/todos', todoController.getTodos);
-router.get('/todo/:id', todoController.getTodo);
+router.get(['/todo', '/todo/:id'], todoController.getTodo);
 router.post('/add-todo', todoController.insertTodo);
 router.patch('/edit-todo/:id', todoController.updateTodo);
 router.delete('/delete-todo/:id', todoController.deleteTodo);

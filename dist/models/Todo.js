30e0293bbd6d63978797d566181d2af964b5b153
kim-eun-ji <qwert101311@gmail.com>
2021-04-09 11:06:25 +0900

    REMOVE outputfolder

--- dist/models/Todo.js
+++
@@ -1,7 +0,0 @@
-//https://medium.com/@shijin_nath/typescript-rest-api-with-express-js-mysql-and-typeorm-8331cea78b0c
-// export class Todo {
-//     id: number;
-//     text: string;
-//     done: boolean;
-// }
-//# sourceMappingURL=Todo.js.map

89f2238aa87a9aed56c81ad17b44902e124e996a
kim-eun-ji <qwert101311@gmail.com>
2021-06-11 14:07:03 +0900

    CONFIG winston looger

--- todo-list/src/loaders/index.ts
+++ todo-list/src/loaders/index.ts
@@ -1,12 +1,18 @@
 import expressLoader from "./express";
 import typeormLoader from "./typeorm";
+import logger from "../config/winston";
 
 export default async ({ expressApp }) => {
-  await typeormLoader();
-  console.log("---------- db Connection");
+  try {
+    await typeormLoader();
+    console.log("---------- db Connection");
 
-  await expressLoader({ app: expressApp });
-  console.log("---------- Express Intialized");
+    await expressLoader({ app: expressApp });
+    console.log("---------- Express Intialized");
+  } catch (e) {
+    logger.error(e.message);
+    throw new Error(e);
+  }
 
   // ... Initialize agenda
   // ... or Redis, or whatever you want

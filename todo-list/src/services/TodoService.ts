45cb5aec9d6d67f7d69dcbdf04523851a7826fd8
kim-eun-ji <qwert101311@gmail.com>
2021-06-15 10:11:39 +0900

    dataMapper 적용

--- todo-list/src/services/TodoService.ts
+++ todo-list/src/services/TodoService.ts
@@ -1,24 +1,36 @@
 import Todos from "../entity/Todos";
-import { TodoRepository } from "../repository/TodoRepository";
-import { BaseService } from "./BaseService";
+import { createConnection } from "typeorm";
+// import { TodoRepository } from "../repository/TodoRepository";
+// import { BaseService } from "./BaseService";
 
 // export default class TodoService extends BaseService {
 export default class TodoService {
-  constructor() {}
+  protected todoRepository;
+
+  constructor() {
+    createConnection().then((c) => (this.todoRepository = c.getRepository(Todos)));
+  }
 
   public list = async (): Promise<Todo[]> => {
-    return await Todos.find();
+    return await this.todoRepository.find();
+    // return await Todos.find();
   };
 
   public view = async (id: number): Promise<Todo> => {
-    return await Todos.findOne(id);
+    return await this.todoRepository.findOne(id);
+    // return await Todos.findOne(id);
+    return null;
   };
 
   public save = async (data: Todos) => {
-    return await data.save();
+    return await this.todoRepository.save(data);
+    // return await data.save();
+    return null;
   };
 
   public remove = async (data: Todos) => {
-    return await data.remove();
+    return await this.todoRepository.remove(data);
+    // return await data.remove();
+    return null;
   };
 }
